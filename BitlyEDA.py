cac18fc7b9bb22d3d25f6783e8d57ccfb0071ee5
ph4nt0mgui1d <aryansharma.bkn@gmail.com>
2021-09-12 18:15:54 +0530

    Updated

--- BitlyEDA.py
+++ BitlyEDA.py
@@ -9,31 +9,24 @@ Created on Mon Jul 19 17:56:31 2021
 import pandas as pd
 import numpy as np
 
-json_df = pd.read_json('Assets/bitly.json', lines = True)
+df = pd.read_json('Assets/bitly.json', lines = True)
 
-json_df.isnull().any(axis = 0)
+df.isnull().any(axis = 0)
+df = df.replace([np.nan, 'Missing'], [' ', 'Unknown'] )
 
-json_df = json_df.replace([np.nan, 'Missing'], [' ', 'Unknown'] )
 
-
-json_df_tz = json_df['tz'].value_counts().head(10)
-
-tz_count = json_df['tz'].value_counts()
-
+df_tz = df['tz'].value_counts().head(10)
+tz_count = df['tz'].value_counts()
 json_df_tz.plot.bar()
 
 
-tokens_df = json_df['a'].str.split(n = 1, expand = True).add_prefix("Token_")
-
+tokens_df = df['a'].str.split(n = 1, expand = True).add_prefix("Token_")
 tokens_frequency = tokens_df['Token_0'].value_counts()
 
 
 tokens_frequency.head().plot.bar()
-
 tokens_df = tokens_df.replace(np.nan, 'Missing')
 
 
-
 tokens_df["os"] = 'Not Windows'
-
 tokens_df["os"][tokens_df["Token_1"].str.find("Windows") != -1] = "Windows"
--- ignAnalysis.py
+++ ignAnalysis.py
@@ -10,21 +10,18 @@ import pandas as pd
 df = pd.read_csv('Assets/ign.csv')
 
 
-xbox_one_filter = (df["score"] > 7) & (df["platform"] == "Xbox One")
-filtered_reviews = df[xbox_one_filter]
-games_list_xbox_one = filtered_reviews['title']
+filtered_reviews = df[(df["score"] > 7) & (df["platform"] == "Xbox One")]
+games_list_xbox = filtered_reviews['title']
 print (games_list_xbox_one)
 
 
-xbox_one = df['platform']=="Xbox One"
-xbox_one_only_df = df[xbox_one]
-xbox_one_reviews = xbox_one_only_df['score_phrase']
-xbox_one_reviews.hist(bins=20, grid = False, xrot=90)
+xbox = df['platform']=="Xbox One"
+xbox_only_df = df[xbox_one]
+xbox_reviews = xbox_one_only_df['score_phrase']
+xbox_reviews.hist(bins=20, grid = False, xrot=90)
 
 
 ps4 = df['platform']=="PlayStation 4"
 ps4_only_df = df[ps4]
 ps4_reviews = ps4_only_df['score_phrase']
 ps4_reviews.hist(bins=20, grid=False, xrot=90)
-
-
